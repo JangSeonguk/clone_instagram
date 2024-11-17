3005a45d73d586232c7a6177a1a8755bb29f3887
James <pcjsu@naver.com>
2024-11-17 22:50:31 +0900

    permission

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:baby_stamp/screen/feed_screen.dart';
 import 'package:baby_stamp/screen/profile_screen.dart';
 import 'package:baby_stamp/constants/screen_size.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -71,8 +72,33 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void _openCamera() {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => const CameraScreen()));
+  void _openCamera() async {
+    // 현재 위젯이 유효한 상태인지 확인하는 조건 추가
+    if (!mounted) return;
+
+    // 권한 확인
+    bool isPermissionGranted = await checkIfPermissionGranted(context);
+
+    // 권한이 허용되었고, 여전히 위젯이 유효한 경우 카메라 화면으로 이동
+    if (isPermissionGranted && mounted) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const CameraScreen()));
+    } else {
+      SnackBar snackBar = SnackBar(
+        content: const Text("사진, 파일, 마이크 접근 권한을 허용해주세요."),
+        action: SnackBarAction(label: "OK", onPressed: () {}),
+      );
+    }
+  }
+
+  Future<bool> checkIfPermissionGranted(BuildContext context) async {
+    Map<Permission, PermissionStatus> statuses =
+        await [Permission.camera, Permission.microphone].request();
+    bool permitted = true;
+
+    statuses.forEach((permission, permissionStatus) {
+      if (!permissionStatus.isGranted) permitted = false;
+    });
+    return permitted;
   }
 }
